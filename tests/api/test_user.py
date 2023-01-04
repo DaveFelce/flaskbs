95616ff27b239d76d462ce2a33a1cbb59642a568
David Felce <davefelce@gmail.com>
2023-01-04 13:30:43 +0000

    fixed stuff

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -1,3 +1,6 @@
-def test_user(client) -> None:
+from flask.testing import FlaskClient
+
+
+def test_user(client: FlaskClient) -> None:
     response = client.get("/user/hank")
     assert response.data == b"Hello, hank"
