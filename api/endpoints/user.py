fca7208453ceda62b75492e7011cfb4765aa6b41
David Felce <davefelce@gmail.com>
2023-01-06 01:22:30 +0000

    added pydantic schema to endpoint for validation

--- api/endpoints/user.py
+++ api/endpoints/user.py
@@ -2,13 +2,18 @@ from typing import Any
 
 from flask import Blueprint
 from flask_pydantic import validate
+from flask_restful import Api, Resource
 
 from schemas.user import UserQueryResponseSchema
 
 bp = Blueprint("user", __name__)
+api = Api(bp)
 
 
-@bp.route("/user/<username>", methods=["GET"])
-@validate()
-def user(username: str) -> Any:
-    return UserQueryResponseSchema(greeting=f"hello, {username}")
+class User(Resource):
+    @validate()
+    def get(self, username: str) -> Any:
+        return UserQueryResponseSchema(greeting=f"hello, {username}")
+
+
+api.add_resource(User, "/user/<username>")
