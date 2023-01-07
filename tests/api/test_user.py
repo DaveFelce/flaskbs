641bbebf0cea408047adb4a832513b35d482eb03
David Felce <davefelce@gmail.com>
2023-01-07 18:28:18 +0000

    added user post test

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -1,10 +1,33 @@
+from http import HTTPStatus
+
 from flask.testing import FlaskClient
+from sqlalchemy.future import select
+from sqlalchemy.orm.scoping import scoped_session
 
 from flaskbs.models import User
 
 
-def test_user(client: FlaskClient, user: User) -> None:
-    response = client.get(f"/user/{user.username}")
+def test_get_user(client: FlaskClient, user: User) -> None:
+    resp = client.get(f"/user/{user.username}")
 
-    assert response
-    assert response.json == {"greeting": f"hello, {user.email}"}
+    assert resp
+    assert resp.json == {"greeting": f"hello, {user.email}"}
+
+
+def test_post_user(client: FlaskClient, db_session: scoped_session) -> None:
+    username = "test-person"
+    email = "testperson@test.com"
+    payload = {
+        "username": username,
+        "email": email,
+    }
+
+    resp = client.post(
+        "/user",
+        json=payload,
+    )
+
+    user = db_session.execute(select(User).where(User.username == username)).scalar_one()
+    assert user.email == email
+
+    assert resp.status_code == HTTPStatus.OK
